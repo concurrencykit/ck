79c7e453ecb864efe21dcfddd7d8219173480fa7
Samy Al Bahra <sbahra@appnexus.com>
2011-10-27 17:20:01 +0000

    ck_epoch: Specify destructor in ck_epoch_retire.

--- include/ck_epoch.h
+++ include/ck_epoch.h
@@ -112,9 +112,10 @@ ck_epoch_end(ck_epoch_record_t *record)
 }
 
 CK_CC_INLINE static void
-ck_epoch_retire(ck_epoch_record_t *record, ck_epoch_entry_t *entry)
+ck_epoch_retire(ck_epoch_record_t *record, ck_epoch_destructor_t destroy, ck_epoch_entry_t *entry)
 {
 
+	entry->destroy = destroy;
 	ck_stack_push_spnc(&record->pending[record->epoch], &entry->stack_entry);
 	record->n_pending += 1;
 	return;
