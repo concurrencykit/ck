f3111b006ba19bf033f461862498c8107adceb00
Samy Al Bahra <sbahra@repnop.org>
2012-09-25 23:51:56 -0400

    ck_fifo: SPSC FIFO dequeue only requires load depends barrier.

--- include/ck_fifo.h
+++ include/ck_fifo.h
@@ -142,9 +142,8 @@ ck_fifo_spsc_dequeue(struct ck_fifo_spsc *fifo, void *value)
 		return (false);
 
 	/* If entry is visible, guarantee store to value is visible. */
-	ck_pr_fence_load();
+	ck_pr_fence_load_depends();
 	ck_pr_store_ptr(value, entry->value);
-
 	ck_pr_fence_store();
 	ck_pr_store_ptr(&fifo->head, entry);
 	return (true);
