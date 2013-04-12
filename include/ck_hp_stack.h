5c483e3b1c951c1509e7a061f88a15ad12787e5d
Samy Al Bahra <sbahra@appnexus.com>
2013-04-11 23:54:16 -0400

    ck_hp: hp_acquire patterns require strict fences under TSO.
    Several counter-examples were found which break in the
    presence of store-to-load re-ordering. Strict fence
    semantics are necessary.
    
    Thanks to Paul McKenney for helpful discussions.

--- include/ck_hp_stack.h
+++ include/ck_hp_stack.h
@@ -62,7 +62,7 @@ ck_hp_stack_pop_mpmc(ck_hp_record_t *record, struct ck_stack *target)
 			return (NULL);
 
 		ck_hp_set(record, 0, entry);
-		ck_pr_fence_memory();
+		ck_pr_fence_strict_memory();
 	} while (entry != ck_pr_load_ptr(&target->head));
 
 	while (ck_pr_cas_ptr_value(&target->head, entry, entry->next, &entry) == false) {
@@ -70,11 +70,11 @@ ck_hp_stack_pop_mpmc(ck_hp_record_t *record, struct ck_stack *target)
 			return (NULL);
 
 		ck_hp_set(record, 0, entry);
-		ck_pr_fence_memory();
+		ck_pr_fence_strict_memory();
 		update = ck_pr_load_ptr(&target->head);
 		while (entry != update) {
 			ck_hp_set(record, 0, update);
-			ck_pr_fence_memory();
+			ck_pr_fence_strict_memory();
 			entry = update;
 			update = ck_pr_load_ptr(&target->head);
 			if (update == NULL)
@@ -95,7 +95,7 @@ ck_hp_stack_trypop_mpmc(ck_hp_record_t *record, struct ck_stack *target, struct 
 		return false;
 
 	ck_hp_set(record, 0, entry);
-	ck_pr_fence_memory();
+	ck_pr_fence_strict_memory();
 	if (entry != ck_pr_load_ptr(&target->head))
 		goto leave;
 
