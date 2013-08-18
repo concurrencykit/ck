1cc189b08ef05c09e7c78eaca6610548b8808e36
Samy Al Bahra <sbahra@repnop.org>
2013-08-17 20:02:41 -0400

    ck_bitmap: Remove unused argument for CK_BITMAP_BITS.
    This is an API change, has been on the queue for a while.
    Patch submitted by Shreyas Prasad <sprasad@appnexus>.

--- include/ck_bitmap.h
+++ include/ck_bitmap.h
@@ -92,7 +92,7 @@
 #define CK_BITMAP_TEST(a, b) \
 	ck_bitmap_test(&(a)->bitmap, (b))
 
-#define CK_BITMAP_BITS(a, b) \
+#define CK_BITMAP_BITS(a) \
 	ck_bitmap_bits(&(a)->bitmap)
 
 #define CK_BITMAP_BUFFER(a) \
