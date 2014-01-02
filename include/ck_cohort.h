16f7568e5c226b10c95066d1c4f6554ead0a5ab1
Samy Al Bahra <sbahra@appnexus.com>
2014-01-02 09:00:45 -0500

    ck_cohort: Migrate to acquire / release.

--- include/ck_cohort.h
+++ include/ck_cohort.h
@@ -108,7 +108,7 @@ enum ck_cohort_state {
 			cohort->acquire_count = 0;				\
 		}								\
 										\
-		ck_pr_fence_memory();						\
+		ck_pr_fence_release();						\
 		LU(cohort->local_lock, local_context);				\
 										\
 		return;								\
