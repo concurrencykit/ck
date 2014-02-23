8f0b88afebda80e2b475d662ab8740bf80136769
Samy Al Bahra <sbahra@repnop.org>
2014-02-23 18:01:02 -0500

    ck_rhs: Localized style(9) clean-up.

--- src/ck_rhs.c
+++ src/ck_rhs.c
@@ -406,6 +406,7 @@ ck_rhs_map_probe_next(struct ck_rhs_map *map,
     unsigned long offset,
     unsigned long probes)
 {
+
 	if (probes & map->offset_mask) {
 		offset = (offset &~ map->offset_mask) + 
 		    ((offset + 1) & map->offset_mask);
@@ -418,6 +419,7 @@ static inline unsigned long
 ck_rhs_map_probe_prev(struct ck_rhs_map *map, unsigned long offset,
     unsigned long probes)
 {
+
 	if (probes & map->offset_mask) {
 		offset = (offset &~ map->offset_mask) + ((offset - 1) & 
 		    map->offset_mask);
@@ -677,7 +679,6 @@ ck_rhs_map_probe(struct ck_rhs *hs,
     unsigned long probe_limit,
     enum ck_rhs_probe_behavior behavior)
 {
-
 	void *k;
 	const void *compare;
 	long pr = -1;
