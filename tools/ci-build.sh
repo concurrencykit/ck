85709d0bc2986296cd34650de85694d23e9d3bde
Samy Al Bahra <sbahra@backtrace.io>
2019-05-12 19:49:10 -0400

    build: move iteration count to ci-build script.

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -4,7 +4,10 @@
 ##############################################################################
 
 set -x
+
+export CFLAGS="-DITERATE=400 -DPAIRS_S=100 -DITERATIONS=24"
 ./configure $@
+
 if [ `uname -s` = "FreeBSD" ]; then
 	make -j $(sysctl -n hw.ncpu)
 else
