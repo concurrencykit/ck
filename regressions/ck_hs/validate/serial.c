5723ec66edc73dc92f5cd7e87629e447a1aa0af5
Samy Al Bahra <sbahra@backtrace.io>
2020-07-02 15:22:00 -0400

    ck_hs: add convenience hash function wrapper ck_hs_hash.
    ck_hs_hash takes two arguments, the hash set and the key, then computes the
    hash value. Performance gain from CK_HS_HASH is negligible with most workloads.
    Basic test coverage in serial.c.
    
    Shoutout to Theo Schlossnagle <jesus@circonus.com> who provided initial patch.

--- regressions/ck_hs/validate/serial.c
+++ regressions/ck_hs/validate/serial.c
@@ -159,7 +159,14 @@ run_test(unsigned int is, unsigned int ad)
 
 	for (j = 0; j < size; j++) {
 		for (i = 0; i < sizeof(test) / sizeof(*test); i++) {
-			h = test[i][0];
+			unsigned long long h_1;
+
+			h = CK_HS_HASH(&hs[j], hs_hash, test[i]);
+			h_1 = ck_hs_hash(&hs[j], test[i]);
+
+			if (h != h_1)
+				ck_error("h != h_1 (%lu != %lu)\n", h, h_1);
+
 			if (ck_hs_get(&hs[j], h, test[i]) != NULL) {
 				continue;
 			}
