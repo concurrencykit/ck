fc0db22de84a5089d376646042f15bdf0c268302
Alex Arslan <ararslan@comcast.net>
2024-08-23 13:52:00 -0700

    build: update CI providers and setup (#228)
    This replaces Drone and Travis with GitHub Actions and an additional
    step in the Cirrus matrix. Travis is no longer posting build statuses to
    this repository and Drone lists every job as "killed" with no
    explanation. GHA and Cirrus are reliable alternatives.
    
    This commit encompasses the following changes:
    - Linux x86-64 with gcc now builds on GHA (was both Travis and Drone)
    - Linux ARM64 with gcc now builds on Cirrus (was Drone)
    - macOS with clang now builds on GHA (was Travis) but it's important to
      note that `macos-latest` uses Apple's ARM-based processors rather than
      the legacy x86-64 Intel processors. That is, the macOS build on CI is
      `arm64-apple-darwin` rather than `x86_64-apple-darwin`.
    - Linux x86-64 with clang now builds on GHA (was Travis)
    - FreeBSD 12.3 is removed from CI as that version is EOL
    - FreeBSD 13.1 is replaced with 13.3
    - FreeBSD 13 and 14 now use release images rather than development
      snapshots. Notably, development snapshot images seem to be culled for
      versions no longer in development, which is why the 12.3 and 13.1
      builds were unable to locate the specified images.
    - `tools/ci-build.sh` now attempts to determine the core count
      automatically. There is too much variability across CI providers for
      `--cores=4` to be universally appropriate; as of this writing, GHA
      uses 4 cores for Linux and 3 for macOS, and Cirrus uses 2.
    - Travis and Drone badges in the README are replaced by GHA
    - Linux PowerPC with clang is no longer run on CI, so it's removed from
      the list in the README
    - GitHub's Dependabot is added in order to automatically keep the
      actions used up to date

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -5,11 +5,31 @@
 
 set -x
 
-export CFLAGS="-DITERATE=400 -DPAIRS_S=100 -DITERATIONS=24 -DSTEPS=10000"
-./configure $@
-
-if [ `uname -s` = "FreeBSD" ]; then
-	make -j $(sysctl -n hw.ncpu)
-else
-	make -j
+# Determine the number of processor cores available but favor a value specified
+# as a command line argument to the script, if provided
+CORES=
+for arg do
+    # Look for `--cores=n` and set `CORES=n` if found
+    if [ `echo "${arg}" | cut -d'=' -f1` = "--cores" ]; then
+        CORES=`echo "${arg}" | cut -d'=' -f2`
+    fi
+done
+ARGS="${@}"
+if [ -z "${CORES}" ]; then
+    # `--cores` not provided, attempt to determine it using appropriate utilities
+    if command -v nproc; then
+        # Linux and FreeBSD 13.2+
+        CORES=`nproc`
+    elif command -v sysctl; then
+        # macOS and BSDs
+        CORES=`sysctl -n hw.ncpu`
+    fi
+    if [ ! -z "${CORES}" ]; then
+        ARGS="${ARGS} --cores=${CORES}"
+    fi
 fi
+
+export CFLAGS="-DITERATE=400 -DPAIRS_S=100 -DITERATIONS=24 -DSTEPS=10000"
+./configure ${ARGS}
+
+make -j${CORES}
