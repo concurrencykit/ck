538875cbd35cc0866cb7e48b0c300bad5143ea4d
Olivier Houchard <cognet@ci0.org>
2026-09-25 18:34:58 +0200

    ck_pr/riscv64: Don't assume long is 64bits
    Similarily to commit 52b52f96998a33efd1576cdeeef357ef6f81ee99, do not
    assume long is 64bits, and use the provided type instead. That way it
    will run if any platform has 32bits long on a 64bits riscv machine.
    
    This should fix github issue #288.

--- include/gcc/riscv64/ck_pr.h
+++ include/gcc/riscv64/ck_pr.h
@@ -111,7 +111,7 @@ CK_PR_FENCE(unlock, CK_FENCE_RW_RW)
 	CK_CC_INLINE static T					\
 	ck_pr_md_load_##S(const M *target)			\
 	{							\
-		long r = 0;					\
+		T r = 0;					\
 		__asm__ __volatile__(I " %0, 0(%1)\n"		\
 					: "=r" (r)		\
 					: "r"  (target)		\
